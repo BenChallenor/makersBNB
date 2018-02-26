c09f87ba378892c54787037b45d2e3cf6c369c05
marcusfgardiner <marcusfgardiner@gmail.com>
2018-02-26 16:30:35 +0000

    Have table being created in database correctly

---
+++ sequilize_test_file.js
@@ -0,0 +1,12 @@
+const Todo = require('server/models').Listings;
+
+module.exports = {
+  create(req, res) {
+    return Listings
+      .create({
+        title: req.body.title,
+      })
+      // .then(todo => res.status(201).send(todo))
+      // .catch(error => res.status(400).send(error));
+  },
+};
