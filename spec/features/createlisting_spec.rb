4963899f77800e972faf55cf5da0d9832591f37b
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2018-02-26 14:48:10 +0000

    Add form and print input

--- spec/features/createlisting_spec.rb
+++ spec/features/createlisting_spec.rb
@@ -1,6 +1,17 @@
 feature 'create listing' do
   scenario 'can fill in the form' do
     visit '/'
-    expect(page).to have_selector(:link_or_button, 'Add Property')
+    expect(page).to have_selector(:link_or_button, 'Add Property!')
+  end
+
+  scenario 'has forms to fill' do
+    visit '/'
+    fill_in 'Name', with: 'Test Name'
+    fill_in 'Phone', with: '078901234567'
+    fill_in 'City', with: 'Chicago'
+    fill_in 'Price_per_night', with: '30£'
+    fill_in 'Description', with: 'cosy, sunny, lots of shops around, good transport'
+    click_button 'Add Property!'
+    expect(page).to have_content 'Chicago'
   end
 end
