d502281d6cc89912d953f6a0160fcb547d9e83e4
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2018-02-28 15:17:07 +0000

    Add forms to index page, can save listing, location, price and description to database

--- controllers/listings.js
+++ controllers/listings.js
@@ -2,9 +2,19 @@ const Listing = require('../models').Listing;
 
 module.exports = {
   create(req, res) {
+    console.log(req);
     return Listing
       .create({
-        name: req.body['listingname']
+        name: req.body['listingname'],
+        location: req.body['listinglocation'],
+        description: req.body['listingdescription'],
+        price: req.body['listingprice']
       })
+  },
+
+  list(req, res) {
+    return Listing
+      .all()
+    .then(listings => res.status(200).send(listings));
   },
 };
