04292f1413103ca0528811e4e81d7fa54c84d074
kotpsot <zygmunt.justyna@gmail.com>
2018-03-02 10:38:23 +0000

    Develop basic get and post routes. Now they handle different messages depending on the success.

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 app.set('view engine', 'ejs');
 
 // app.use(express.static(__dirname + "/public"));
-app.use("/public",express.static(__dirname + "/public"));
+app.use("/public",express.static(__dirname + "/public")); // put jquery (and other static things like css and images) to public and load it
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,8 +17,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
 
 require('./routes')(app);
-app.get('*', (req, res) => res.status(200).send({
-  message: 'Welcome to the beginning of nothingness.',
+app.get('*', (req, res) => res.status(404).send({
+  message: 'Not found.',
 }));
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,32 @@ debugger;
 const listingsController = require('../controllers').listings;
 var path = require('path');
 
+function renderResponse(req, res, message) {
+  listingsController.list(req, res)
+   .then(function(listings){
+      res.render('index', { x: listings, message: message });
+    });
+}
 
 module.exports = function(app) {
 
   app.get('/', function(req, res) {
-     listingsController.list(req, res)
-      .then(function(listings){
-         res.render('index', { x: listings })
-       });
+     renderResponse(req, res, "");
    });
 
-  app.post('/', listingsController.create);
+   app.post('/', function(req, res) {
+      if (isNaN(parseInt(req.body['listingprice']))) {
+        renderResponse(req, res, "put a number in the price plz.");
+        return;
+      }
+      listingsController.create(req, res)
+       .then(function(listing){ // redirect when success, flash needed.
+          res.redirect(302, '/');
+        }, function(error) { // render with error on failure
+          renderResponse(req, res, "Error!");
+        });
+    });
+
+  // app.post('/', listingsController.create);
 
 }
