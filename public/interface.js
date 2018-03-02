76b18ce60518ad2e2822ab2a03df943968de6b4e
kotpsot <zygmunt.justyna@gmail.com>
2018-03-02 12:23:33 +0000

    appearing form ready

--- public/interface.js
+++ public/interface.js
@@ -3,7 +3,7 @@ $( document ).ready(function() {
   $('.welcome2').text("YO!!!");
 
   $('#add_place_button').click(function() {
-    $('.form').attr('class', 'invisible_form')
+    $('.form').attr('class', 'visible_form')
   });
 
 });
--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 var Browser = require('zombie');
 Browser.localhost('localhost.com', 3000)
 var browser = new Browser(); // zombie allows to create this fake browser.
-// on line 3 we create a new Browser. It knows where to look for content thanks to line 2. 
+// on line 3 we create a new Browser. It knows where to look for content thanks to line 2.
 
 describe('Homepage', function() {
 
